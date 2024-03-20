431633e39df78b29b3835fbb8a88d06e51d5815f
sai <girgaonkar.chaitanya@gmail.com>
2024-03-20 16:26:43 +0530

    final

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ app.use(cookieParser())
 import userRouter from './routes/user.router.js'
 import profileRouter from "./routes/profile.router.js"
 import reportRouter from "./routes/report.router.js"
-
+import allergyRouter from "./routes/allergy.router.js"
+import scheduleRouter from "./routes/schedule.router.js"
 
 // routes declaration 
 app.use("/api/v1/user" , userRouter)
@@ -29,7 +30,8 @@ app.use("/api/v1/profile", profileRouter)
 
 app.use("/api/v1/report", reportRouter)
 
-
+app.use("/api/v1/allergy", allergyRouter)
+app.use("/api/v1/schedule", scheduleRouter)
 
 
 
